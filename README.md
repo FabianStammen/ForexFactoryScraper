51dd31c33e6fb3984def19f901f7b034c64baa0b
FabianStammen <f.stammen@student.fontys.nl>
2019-09-06 16:58:55 +0200

    switched to selenium to get consistent Timezone and daysaving time values, this may slow down the process but gives consistency

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 A simple web scraper that downloads forex calendar data from ForexFactory.
 
 The download is managed in packets of weeks, stating on a sunday.\
+The chosen timezone is Eastern Standard Time (EST) time-zone WITHOUT Day Light Savings adjustments, to mach the data from https://www.histdata.com\.\
 The program starts at the first possible download date, jan 7 2007, and ends with the last completed week.\
 Any later executions of the program checks for the date of the last entries and updates the list incremental.\
 The out put file will be located in the working directory and be called forex_factory_catalog.csv
 
-All requirements are within requirements.txt.\
+It is required to have firefox installed and a version of geckodriver in the same folder as the program. (https://github.com/mozilla/geckodriver/releases\).\
+All other requirements are within requirements.txt.\
 You can easily install them with 'pip install -r requirements.txt'.
--- ffs.py
+++ ffs.py
@@ -3,8 +3,8 @@ import logging
 from datetime import date, datetime, timedelta
 from os import path
 
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 
 def set_logger():
@@ -18,76 +18,77 @@ def set_logger():
     logging.getLogger('').addHandler(console)
 
 
-def get_economic_calendar(start_link, end_link):
-    # write to console current status
-    logging.info("Scraping data for link: {}".format(start_link))
+def setup_driver_with_timezone():
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+    driver.get('https://www.forexfactory.com/timezone.php')
+    driver.find_element_by_xpath('//select[@name="timezoneoffset"]/option[@value="-5"]').click()
+    driver.find_element_by_xpath('//select[@name="dst"]/option[@value="0"]').click()
+    driver.find_element_by_xpath('//select[@name="options[timeformat]"]/option[@value="1"]').click()
+    driver.find_element_by_xpath('//input[@value="Save Settings"]').click()
+    return driver
 
-    # get the page and make the soup
-    base_url = "https://www.forexfactory.com/"
-    r = requests.get(base_url + start_link)
-    data = r.text
-    soup = BeautifulSoup(data, "lxml")
 
-    # get and parse table data, ignoring details and graph
-    table = soup.find("table", class_="calendar__table")
+def scrap(start_link, end_link, driver):
+    logging.info('Scraping data for link: {}'.format(start_link))
+    base_url = 'https://www.forexfactory.com/'
 
-    # do not use the ".calendar__row--grey" css selector (reserved for historical data)
-    trs = table.select("tr.calendar__row.calendar_row")
-    fields = ["date", "time", "currency", "impact", "event", "actual", "forecast", "previous"]
+    driver.get(base_url + start_link)
+    driver.find_elements_by_class_name('calendar__table')
+
+    trs = driver.find_elements_by_css_selector('tr.calendar__row.calendar_row')
+    # trs = table.select('tr.calendar__row.calendar_row')
+    fields = ['date', 'time', 'currency', 'impact', 'event', 'actual', 'forecast', 'previous']
 
     # some rows do not have a date (cells merged)
     curr_year = start_link[-4:]
-    curr_date = ""
-    curr_time = ""
+    curr_date = ''
+    curr_time = ''
     for tr in trs:
-
         # fields may mess up sometimes, see Tue Sep 25 2:45AM French Consumer Spending
         # in that case we append to errors.csv the date time where the error is
         try:
             for field in fields:
-                data = tr.select("td.calendar__cell.calendar__{}.{}".format(field, field))[0]
-                # print(data)
-                if field == "date" and data.text.strip() != "":
-                    curr_date = data.text.strip()
-                elif field == "time" and data.text.strip() != "":
-                    # time is sometimes "All Day" or "Day X" (eg. WEF Annual Meetings)
-                    if data.text.strip().find("Day") != -1:
-                        curr_time = "12:00am"
+                data = tr.find_elements_by_css_selector('td.calendar__cell.calendar__{}.{}'.format(field, field))[0]
+                if field == 'date' and data.text.strip() != '':
+                    curr_date = data.text.strip().replace('\n', '')
+                elif field == 'time' and data.text.strip() != '':
+                    if data.text.strip().find('Day') != -1:
+                        curr_time = '00:00'
                     else:
                         curr_time = data.text.strip()
-                elif field == "currency":
+                elif field == 'currency':
                     currency = data.text.strip()
-                elif field == "impact":
-                    # when impact says "Non-Economic" on mouseover, the relevant
-                    # class name is "Holiday", thus we do not use the classname
-                    impact = data.find("span")["title"]
-                elif field == "event":
+                elif field == 'impact':
+                    impact = data.find_element_by_css_selector('span')
+                    impact = impact.get_attribute('title')
+                elif field == 'event':
                     event = data.text.strip()
-                elif field == "actual":
+                elif field == 'actual':
                     actual = data.text.strip()
-                elif field == "forecast":
+                elif field == 'forecast':
                     forecast = data.text.strip()
-                elif field == "previous":
+                elif field == 'previous':
                     previous = data.text.strip()
 
-            dt = datetime.strptime(",".join([curr_year, curr_date, curr_time]),
-                                   "%Y,%a%b %d,%I:%M%p")
+            dt = datetime.strptime(','.join([curr_year, curr_date, curr_time]),
+                                   '%Y,%a%b %d,%H:%M')
             with open('forex_factory_catalog.csv', 'a') as file:
-                file.write(",".join([str(dt), currency, impact, event, actual, forecast, previous])
+                file.write(','.join([str(dt), currency, impact, event, actual, forecast, previous])
                            + '\n')
         except:
-            with open("errors.csv", "a") as f:
+            with open('errors.csv', 'a') as f:
                 csv.writer(f).writerow([curr_year, curr_date, curr_time])
 
-    # exit recursion when last available link has reached
     if start_link == end_link:
-        logging.info("Successfully retrieved data")
+        logging.info('Successfully retrieved data')
         return
 
-    # get the link for the next week and follow
-    follow = soup.select("a.calendar__pagination.calendar__pagination--next.next")
-    follow = follow[0]["href"]
-    get_economic_calendar(follow, end_link)
+    follow = driver.find_element_by_css_selector(
+        'a.calendar__pagination.calendar__pagination--next.next').get_attribute('href')
+    follow = follow[follow.rindex('/')+1:]
+    scrap(follow, end_link, driver)
 
 
 def date_to_str(day):
@@ -103,7 +104,7 @@ def get_start_date_str():
     if path.exists('forex_factory_catalog.csv'):
         with open('forex_factory_catalog.csv', 'rb') as file:
             file.seek(-2, 2)
-            while file.read(1) != b"\n":
+            while file.read(1) != b'\n':
                 file.seek(-2, 1)
             last_day = file.readline()[:10].decode()
             start_day = date(int(last_day[:4]), int(last_day[5:7]), int(last_day[8:10])) \
@@ -119,7 +120,8 @@ def get_start_date_str():
     return date_to_str(start_day)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     set_logger()
-    get_economic_calendar('calendar.php?week=' + get_start_date_str(),
-                          'calendar.php?week=' + get_end_date_str())
+    scrap('calendar.php?week=' + get_start_date_str(),
+          'calendar.php?week=' + get_end_date_str(),
+          setup_driver_with_timezone())
