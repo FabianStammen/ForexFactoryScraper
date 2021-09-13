a89585ec8601bb34079246d88a6ddef6a70fac9d
FabianStammen <f.stammen@yahoo.de>
2021-09-13 10:21:10 +0200

    updated detection to also delete duplicates memory efficient

--- dataset_util.py
+++ dataset_util.py
@@ -1,14 +1,37 @@
 """Dataset Util
-A script that checks for Non-Economic duplicates in the dataset
+A script that finds and deletes Non-Economic duplicates in the dataset
 """
 import csv
+import os
+
+
+def delete_multiple_lines(original_file, line_numbers):
+    """In a file, delete the lines at line number in given list"""
+    is_skipped = False
+    if not line_numbers:
+        return
+    counter = 0
+    dummy_file = original_file + '.bak'
+    with open(original_file, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        for line in read_obj:
+            if counter not in line_numbers:
+                write_obj.write(line)
+            else:
+                is_skipped = True
+            counter += 1
+    if is_skipped:
+        os.remove(original_file)
+        os.rename(dummy_file, original_file)
+    else:
+        os.remove(dummy_file)
+
 
 if __name__ == '__main__':
     count = dict()
-    lockAt = list()
+    lineNumbers = list()
     with open('forex_factory_catalog.csv', mode='r') as file:
         reader = csv.reader(file, delimiter=',')
-        for row in reader:
+        for i, row in enumerate(reader):
             if row[2] != 'Non-Economic':
 
                 if row[0] not in count:
@@ -18,9 +41,6 @@ if __name__ == '__main__':
                 elif row[1] not in count[row[0]][row[3]]:
                     count[row[0]][row[3]][row[1]] = 0
                 else:
-                    lockAt.append(row)
+                    lineNumbers.append(i)
                 count[row[0]][row[3]][row[1]] = count[row[0]][row[3]][row[1]] + 1
-
-    # od = OrderedDict(sorted(count.items(), key=lambda x:x[1], reverse=True))
-    for i, problem in enumerate(lockAt):
-        print(str(i) + ': ' + str(problem) + ' : ' + str(count[problem[0]][problem[3]][problem[1]]))
+    delete_multiple_lines(original_file='forex_factory_catalog.csv', line_numbers=lineNumbers)
